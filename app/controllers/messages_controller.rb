b49860d6f7a9de11437f0215ccc26a3df4e389aa
Sann Chhan <sann.c.chhan@gmail.com>
2016-05-05 12:35:25 -0700

    Add New and Create actions with views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,4 +3,26 @@ class MessagesController < ApplicationController
 	def index
 	end
 
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(message_params)
+
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:title, :description)
+	end
+
+	def find_message
+	end
 end
